ac7beffae904dd81e39b11c11996b70d4caa977b
karthikeyan-codes07 <karthikchow9866@gmail.com>
2026-07-22 20:32:41 +0530

    Add Sqrt(x) Binary Search solution

---
+++ Maths/SqrtX.java
@@ -0,0 +1,39 @@
+/*
+Problem: Sqrt(x)
+Platform: LeetCode
+Approach: Binary Search
+Time Complexity: O(log n)
+Space Complexity: O(1)
+Status: Accepted
+*/
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0 || x==1)
+        {
+            return x;
+        }
+        int mid;
+        int low=1;
+        int high=x;
+        int ans = 0;
+        while(low<=high)
+        {
+            mid = low+(high-low)/2;
+            if((long)mid * mid == x)
+            {
+                return mid;
+            }
+            else if((long)mid *mid <x)
+            {
+                ans = mid;
+                low = mid+1;
+            }
+            else 
+            {
+                high= mid-1;
+            }
+        }
+        return ans;
+    }
+}
